aa83f14f55d1c1dc60dca153bab99d645d825ac8
vibhudadhichi <vibhudadhikhi@gmail.com>
2016-09-13 21:20:33 +0530

    Initial commit

---
+++ app/src/main/java/com/vibhu/placesmaps/MainActivity.java
@@ -0,0 +1,27 @@
+package com.vibhu.placesmaps;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        final ArrayList<String> places = new ArrayList<String >();
+
+        places.add("Add a new Place");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,places);
+
+        listView.setAdapter(arrayAdapter);
+    }
+}
